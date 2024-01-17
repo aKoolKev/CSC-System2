60d7924b7ae678c3b279a465ee55ef91a5c33d25
aKoolKev <kevindong0212@gmail.com>
2024-01-16 18:58:51 -0600

    Mapped ALPHA and Handle whitespace

--- Programming-Projects/Project 1/Project1Provided/Token.cpp
+++ Programming-Projects/Project 1/Project1Provided/Token.cpp
@@ -13,6 +13,28 @@
 // the 2d array (table)
 static int **DFA = nullptr;
 
+#define ERROR -1
+#define START 0
+#define ALPHA 1
+#define NUM_INT 2
+#define NUM_REAL 4
+#define ADDOP 5
+#define MULOP 6
+
+//questionable
+#define RELOP 7
+#define RELOP 8
+
+#define ASSIGNOP 9
+#define LPAREN 10
+#define RPAREN 11
+#define AND 13
+#define OR 15
+#define SEMICOLON 16
+#define LBRACK 17
+#define RBRACK 18
+#define COMMA 19
+
 using namespace std;
 
 // the promised global for string equivalents of TokenType enumeration
@@ -63,7 +85,7 @@ void Token::get(istream &is)
   //DFA table does not exists yet ...
   if (!DFA)
   {
-    DFA = new int*[160];                         ////Unsure on DFA table size... 16 final states 
+    DFA = new int*[19+1];                         ////Unsure on DFA table size... 19 states plus one for start
     
     //initalize table to all ERRORS 
     for (int state=0; state <=16; state++) //row
@@ -76,44 +98,70 @@ void Token::get(istream &is)
     
 
     // all transitions from start state (DO NOT KNOW THE ENUM)
-    DFA[0/*Need to replace*/][ (int) ',' ] = COMMA;
-    DFA[0/*Need to replace*/][ (int) ';' ] = SEMICOLON;
-    DFA[0/*Need to replace*/][ (int) ']' ] = RBRACK;
-    DFA[0/*Need to replace*/][ (int) '[' ] = LBRACK;
-    DFA[0/*Need to replace*/][ (int) '(' ] = LPAREN;
-    DFA[0/*Need to replace*/][ (int) ')' ] = RBRACK;
+    DFA[0/*Need to replace*/][ (int) ',' ] = COMMA;         //START -> ,
+    DFA[0/*Need to replace*/][ (int) ';' ] = SEMICOLON;     //START -> ;
+    DFA[0/*Need to replace*/][ (int) ']' ] = RBRACK;        //START -> ]
+    DFA[0/*Need to replace*/][ (int) '[' ] = LBRACK;        //START -> [
+    DFA[0/*Need to replace*/][ (int) '(' ] = LPAREN;        //START -> (
+    DFA[0/*Need to replace*/][ (int) ')' ] = RBRACK;        //START -> )
 
-
+    //START -> =
     DFA[0/*Need to replace*/][ (int) '=' ] = ASSIGNOP;
+                                                        // ==
                                                         DFA[ASSIGNOP][ (int) '=' ] = RELOP;
 
-    //unsure
+    //unsure: START to DIGIT
     for (char ch='0'; ch <= '9'; ch++)
-      DFA[0/*Need to replace*/][ (int) ch ] = NUM_INT;
+      DFA[0/*Need to replace*/][ (int) ch ] = NUM_INT;  // DIGIT to DIGIT
+                                                        for (char ch='0'; ch<='9'; ch++)
+                                                          DFA[NUM_INT][ (int) ch] = NUM_INT;
+                                                                                              // DIGIT to FLOAT
+                                                                                              DFA[NUM_INT][ (int) '.' ] = NUM_REAL; 
+                                                                                                                                      // FLOAT to DIGIT
+                                                                                                                                      for (char ch='0'; ch<='9'; ch++)
+                                                                                                                                        DFA[NUM_REAL][ (int) ch ] = NUM_INT;
 
-                                                        DFA[NUM_INT][ (int) '.' ] = NUM_REAL;
-                                                                                                for (char ch='0'; ch<='9'; ch++)
-                                                                                                  DFA[NUM_REAL][ (int) ch ] = NUM_INT;
 
+    //unsure: START to ALPHA (lowercase)
+    for (char ch='a'; ch <= 'z'; ch++)
+        DFA[0/*Need to replace*/][ (int) ch ] = ID;     // at ALPHA to ALPHA
+                                                        for (char ch='a'; ch <= 'z'; ch++) 
+                                                          DFA[ID][ (int) ch ] = ID;
+
+                                                        // at ALPHA to DIGIT
+                                                        for(char ch='0'; ch<='9'; ch++) 
+                                                          DFA[ID][ (int) ch] = NUM_INT;
+
+
+    //unsure: START to ALPHA (Uppercase)
+    for (char ch='A'; ch <= 'Z'; ch++)
+        DFA[0/*Need to replace*/][ (int) ch ] = ID;     // at ALPHA to ALPHA
+                                                        for (char ch='A'; ch <= 'Z'; ch++) 
+                                                          DFA[ID][ (int) ch ] = ID;
+
+                                                        // at ALPHA to DIGIT
+                                                        for(char ch='0'; ch<='9'; ch++) 
+                                                          DFA[ID][ (int) ch] = NUM_INT;
+
+
+    //unsure: START -> '<'
+    DFA[0/*Need to replace*/][ (int) '<' ] = RELOP;     // <=
+                                                        DFA[RELOP /* '<' */][ (int) '=' ] = RELOP;
+
+
+    //unsure: START -> '>'  
+    DFA[0/*Need to replace*/][ (int) '>' ] = RELOP;     // >=
+                                                        DFA[RELOP /* '>' */][ (int) '=' ] = RELOP;
 
     //unsure
-    DFA[0/*Need to replace*/][ (int) '<' ] = RELOP;
-                                                        DFA[RELOP][ (int) '=' ] = RELOP;
+    DFA[0/*Need to replace*/][ (int) '*' ] = MULOP;     //START -> *
+    DFA[0/*Need to replace*/][ (int) '/' ] = MULOP;     //START -> /
+    DFA[0/*Need to replace*/][ (int) '+' ] = ADDOP;     //START -> +
+    DFA[0/*Need to replace*/][ (int) '-' ] = ADDOP;     //START -> -
 
-
-    //unsure
-    DFA[0/*Need to replace*/][ (int) '>' ] = RELOP;
-                                                        DFA[RELOP][ (int) '=' ] = RELOP;
-
-    //unsure
-    DFA[0/*Need to replace*/][ (int) '*' ] = MULOP;
-    DFA[0/*Need to replace*/][ (int) '/' ] = MULOP;
-    DFA[0/*Need to replace*/][ (int) '+' ] = ADDOP;
-    DFA[0/*Need to replace*/][ (int) '-' ] = ADDOP;
-
-
-    DFA[0/*Need to replace*/][ (int) '|' ] = OR;
-    DFA[0/*Need to replace*/][ (int) '&' ] = AND;
+    //UNSURE!
+    DFA[0/*Need to replace*/][ (int) '|' /*'||'*/ ] = OR;        //START -> ||
+    DFA[0/*Need to replace*/][ (int) '&' /*'&&'*/ ] = AND;       //START -> &&
 
   } //finish initializing table
   
@@ -122,6 +170,19 @@ void Token::get(istream &is)
   char ch;
 
   // ** should probably think about skipping white space here **
+  ch = is.get();// get next char from input
+  while (isspace(ch)) // || is isComment() ...
+  {
+    if (ch=='\n') // char read was a new line
+      _line_num++;
+
+    ch = is.get();
+  }
+  if (!is)
+    return is; //NEED TO CHANGE
+
+  is.putback(ch); 
+
 
   //keep track of the state
   int currState = 0; /*PLACE HOLDER*/          // ** still do not know the number for start state
