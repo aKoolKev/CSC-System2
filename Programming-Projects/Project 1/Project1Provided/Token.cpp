2c6e2039969f6af2b230b501c5a19042ded092c9
aKoolKev <kevindong0212@gmail.com>
2024-01-17 18:08:02 -0600

    Use integer values from graph rather than using enum constant value.

--- Programming-Projects/Project 1/Project1Provided/Token.cpp
+++ Programming-Projects/Project 1/Project1Provided/Token.cpp
@@ -13,27 +13,6 @@
 // the 2d array (table)
 static int **DFA = nullptr;
 
-#define ERROR -1
-#define START 0
-#define ALPHA 1
-#define NUM_INT 2
-#define NUM_REAL 4
-#define ADDOP 5
-#define MULOP 6
-
-//questionable
-#define RELOP 7
-#define RELOP 8
-
-#define ASSIGNOP 9
-#define LPAREN 10
-#define RPAREN 11
-#define AND 13
-#define OR 15
-#define SEMICOLON 16
-#define LBRACK 17
-#define RBRACK 18
-#define COMMA 19
 
 using namespace std;
 
@@ -48,6 +27,30 @@ string TokStr[]=
 static string reserved[]={"int" , "float", "while", "if", "then", "else", "void", "begin", "end" };
 
 
+// helper function that returns the Token Type of a Final State number
+string getTokenTypeByNum (int finalStateNum)
+{
+  switch (finalStateNum)
+  {
+    case 1: return "ID";
+    case 2: return "NUM_INT";
+    case 4: return "NUM_REAL";
+    case 5: return "ADDOP";
+    case 6: return "MULOP";
+    case 7: return "RELOP";
+    case 8: return "RELOP";
+    case 9: return "ASSIGNOP";
+    case 10: return "LPAREN";
+    case 11: return "RPAREN";
+    case 13: return "AND";
+    case 15: return "OR";
+    case 16: return "SEMICOLON";
+    case 17: return "LBRACK";
+    case 18: return "RBRACK";
+    case 19: return "COMMA";
+  }
+  cout << "ERROR: " << finalStateNum << " is invalid!\n\n";
+}
 
 /******************************************************
  *  just prints out the info describing this Token    *
@@ -85,83 +88,90 @@ void Token::get(istream &is)
   //DFA table does not exists yet ...
   if (!DFA)
   {
-    DFA = new int*[19+1];                         ////Unsure on DFA table size... 19 states plus one for start
+    DFA = new int*[19+1]; // 19 states + the start state
     
-    //initalize table to all ERRORS 
-    for (int state=0; state <=16; state++) //row
+    // setting up DFA table
+    for (int state=0; state <=20; state++) //row
     {
-      DFA[state] = new int[256];                ////Unsure on array size s
+      DFA[state] = new int[256]; // new column at each row (state) that contains all the possible characters
 
-      for (int ch=0; ch<256; ch++) // col
-        DFA[state][ch] = ERROR; 
+      //initalize each column to ERROR
+      for (int ch=0; ch<256; ch++)
+        DFA[state][ch] = -1; // -1 = ERROR
     }
     
+    // Mapping out graph 
 
-    // all transitions from start state (DO NOT KNOW THE ENUM)
-    DFA[0/*Need to replace*/][ (int) ',' ] = COMMA;         //START -> ,
-    DFA[0/*Need to replace*/][ (int) ';' ] = SEMICOLON;     //START -> ;
-    DFA[0/*Need to replace*/][ (int) ']' ] = RBRACK;        //START -> ]
-    DFA[0/*Need to replace*/][ (int) '[' ] = LBRACK;        //START -> [
-    DFA[0/*Need to replace*/][ (int) '(' ] = LPAREN;        //START -> (
-    DFA[0/*Need to replace*/][ (int) ')' ] = RBRACK;        //START -> )
-
-    //START -> =
-    DFA[0/*Need to replace*/][ (int) '=' ] = ASSIGNOP;
-                                                        // ==
-                                                        DFA[ASSIGNOP][ (int) '=' ] = RELOP;
-
-    //unsure: START to DIGIT
-    for (char ch='0'; ch <= '9'; ch++)
-      DFA[0/*Need to replace*/][ (int) ch ] = NUM_INT;  // DIGIT to DIGIT
-                                                        for (char ch='0'; ch<='9'; ch++)
-                                                          DFA[NUM_INT][ (int) ch] = NUM_INT;
-                                                                                              // DIGIT to FLOAT
-                                                                                              DFA[NUM_INT][ (int) '.' ] = NUM_REAL; 
-                                                                                                                                      // FLOAT to DIGIT
-                                                                                                                                      for (char ch='0'; ch<='9'; ch++)
-                                                                                                                                        DFA[NUM_REAL][ (int) ch ] = NUM_INT;
+    DFA[0][ (int) ',' ] = 19;   // START (0) -> , (19)
+    DFA[0][ (int) ';' ] = 16;   // START (0) -> ; (16)
+    DFA[0][ (int) ']' ] = 18;   // START (0) -> ] (18)
+    DFA[0][ (int) '[' ] = 17;   // START (0) -> [ (17)
+    DFA[0][ (int) '(' ] = 10;   // START (0) -> ( (10)
+    DFA[0][ (int) ')' ] = 11;   // START (0) -> ) (11)
 
 
-    //unsure: START to ALPHA (lowercase)
-    for (char ch='a'; ch <= 'z'; ch++)
-        DFA[0/*Need to replace*/][ (int) ch ] = ID;     // at ALPHA to ALPHA
-                                                        for (char ch='a'; ch <= 'z'; ch++) 
-                                                          DFA[ID][ (int) ch ] = ID;
+    DFA[0][ (int) '*' ] = 6;     // START (0) -> * (6)
+    DFA[0][ (int) '/' ] = 6;     // START (0) -> / (6)
+    DFA[0][ (int) '+' ] = 5;     // START (0) -> + (5)
+    DFA[0][ (int) '-' ] = 5;     // START (0) -> - (5)
 
-                                                        // at ALPHA to DIGIT
-                                                        for(char ch='0'; ch<='9'; ch++) 
-                                                          DFA[ID][ (int) ch] = NUM_INT;
-
-
-    //unsure: START to ALPHA (Uppercase)
-    for (char ch='A'; ch <= 'Z'; ch++)
-        DFA[0/*Need to replace*/][ (int) ch ] = ID;     // at ALPHA to ALPHA
-                                                        for (char ch='A'; ch <= 'Z'; ch++) 
-                                                          DFA[ID][ (int) ch ] = ID;
-
-                                                        // at ALPHA to DIGIT
-                                                        for(char ch='0'; ch<='9'; ch++) 
-                                                          DFA[ID][ (int) ch] = NUM_INT;
+  
+    DFA[0][ (int) '=' ] = 9;    // START (0) -> = (9)
+      DFA[9][ (int) '=' ] = 8;    // = (9) -> = (8)
 
 
-    //unsure: START -> '<'
-    DFA[0/*Need to replace*/][ (int) '<' ] = RELOP;     // <=
-                                                        DFA[RELOP /* '<' */][ (int) '=' ] = RELOP;
+    DFA[0][ (int) '<' ] = 7;    // START (0) -> < (7)
+      DFA[7][ (int) '=' ] = 8;    // < (7) -> = (8)
+
+    DFA[0][ (int) '>' ] = 7;    // START (0) -> > (7)
+      DFA[7] [ (int) '=' ] = 8;   // > (7) -> = (8)
 
 
-    //unsure: START -> '>'  
-    DFA[0/*Need to replace*/][ (int) '>' ] = RELOP;     // >=
-                                                        DFA[RELOP /* '>' */][ (int) '=' ] = RELOP;
+    DFA[0][ (int) '|' ] = 14;   // START (0) -> | (14)
+      DFA[14][ (int) '|' ] = 15;    // | (14) -> | (15)
+    
+    DFA[0][ (int) '&' ] = 12;   // START (0) -> & (12)
+      DFA[12] [ (int) '&' ] = 13;    //START (0) -> & (13)
 
-    //unsure
-    DFA[0/*Need to replace*/][ (int) '*' ] = MULOP;     //START -> *
-    DFA[0/*Need to replace*/][ (int) '/' ] = MULOP;     //START -> /
-    DFA[0/*Need to replace*/][ (int) '+' ] = ADDOP;     //START -> +
-    DFA[0/*Need to replace*/][ (int) '-' ] = ADDOP;     //START -> -
 
-    //UNSURE!
-    DFA[0/*Need to replace*/][ (int) '|' /*'||'*/ ] = OR;        //START -> ||
-    DFA[0/*Need to replace*/][ (int) '&' /*'&&'*/ ] = AND;       //START -> &&
+    // could optimize...
+    for (char ch='0'; ch<='9'; ch++)
+      DFA[0][ (int) ch ] = 2;   // START (0) -> 0,1,...,9 denoted are DIGIT = 2
+
+      for (char ch='0'; ch<='9'; ch++)
+        DFA[2][ (int) ch ] = 2;   // DIGIT (2) -> DIGIT (2) 
+
+      for (char ch='0'; ch<='9'; ch++)
+        DFA[2] [ (int) '.' ] = 3;   // DIGIT (2) -> . (3)
+
+          for (char ch='0'; ch<='9'; ch++)
+            DFA[3][ (int) ch ] = 4;   // . (3) -> DIGIT (4)
+
+            for (char ch='0'; ch<='9'; ch++)
+              DFA[4][ (int) ch ] = 4;   // DIGIT (4) -> DIGIT (4)
+
+                                                                                                                            
+    // could optimize... (lowercase letters)
+    for (char ch='a'; ch<='z'; ch++)
+      DFA[0][ (int) ch ] = 1;   // START (0) -> ALPHA (1)
+    
+      for (char ch='a'; ch<='z'; ch++)
+        DFA[1][ (int) ch ] = 1;   // ALPHA (1) -> ALPHA (1)
+    
+      for (char digit='0'; digit<='9'; digit++)
+        DFA[1][ (int) digit ] = 1;  //ALPHA (1) -> DIGIT (1)
+
+
+    // could optimize... (uppercase letters)
+    for (char ch='A'; ch<='Z'; ch++)
+      DFA[0][ (int) ch ] = 1;   // START (0) -> ALPHA (1)
+    
+      for (char ch='A'; ch<='Z'; ch++)
+        DFA[1][ (int) ch ] = 1;   // ALPHA (1) -> ALPHA (1)
+    
+      for (char digit='0'; digit<='9'; digit++)
+        DFA[1][ (int) digit ] = 1;  //ALPHA (1) -> DIGIT (1)
+
 
   } //finish initializing table
   
@@ -179,17 +189,17 @@ void Token::get(istream &is)
     ch = is.get();
   }
   if (!is)
-    return is; //NEED TO CHANGE
+    // _type = TokenType[];                    //NEED TO FIX
 
   is.putback(ch); 
 
 
   //keep track of the state
   int currState = 0; /*PLACE HOLDER*/          // ** still do not know the number for start state
-  int prevState = ERROR;  // at start, no previous state
+  int prevState = -1;  // at start, no previous state
 
   //the algorithm
-  while(currState!=ERROR)
+  while(currState!=-1)
   {
     ch = is.get(); // get next char from input
 
@@ -197,13 +207,13 @@ void Token::get(istream &is)
     prevState = currState;
     currState = DFA[currState][(int) ch];
 
-    if (currState!=ERROR) // if character is a valid part of token ...
+    if (currState!=-1) // if character is a valid part of token ...
       _value+=ch; // ... add char to lexem's value
 
   }
   
   // hopped out because next 
-  _type = TokenType(prevState);       // not sure if I can type cast this 
+  _type = TokenType(prevState);       //NOT SURE HOW TO DO, BUT NEED TO FIX
 
   // we read an extra character ... put it back for the next read()
   if (is) // insure we are not at the end of the line
