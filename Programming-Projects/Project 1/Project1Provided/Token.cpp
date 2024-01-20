9075352a180572f307cb77b1c3c948bf002ae6f4
aKoolKev <kevindong0212@gmail.com>
2024-01-19 18:11:01 -0600

    Finalized Project1.

--- Programming-Projects/Project 1/Project1Provided/Token.cpp
+++ Programming-Projects/Project 1/Project1Provided/Token.cpp
@@ -1,16 +1,16 @@
-/*******************************************
- * File: Token.cpp                         *
- * Author: S. Blythe                       *
- * Date: 12/2023                           *
- * PURPOSE: implementation for Token       *
- *******************************************/
+/****************************************************
+ * File: Token.cpp                                  *
+ * Author: S. Blythe & K. Dong                      *
+ * Date: 1/11/2024                                  *
+ * PURPOSE: implementation for Token                *
+ ***************************************************/
 
 #include "Token.hpp"
 
 #include <fstream>
 #include <iomanip>
 
-// the 2d array (table)
+// the 2d array table
 static int **DFA = nullptr;
 
 
@@ -28,19 +28,19 @@ static string reserved[]={"int" , "float", "while", "if", "then", "else", "void"
 
 
 
-
 // helper function that indicate if a provided character is a '#'
 bool isComment(char ch)
 {
   bool val = false;
 
-  if (ch == '#')
+  if (ch == '#') // a comment!
     val = true;
 
   return val;
 }
 
-// helper function that returns the Token Type of a given final state number (based on graph)
+
+// helper function that returns the TokenType of a given final state number (based on graph numbers)
 TokenType getTokenTypeByNum (int finalStateNum)
 {
   switch (finalStateNum)
@@ -63,29 +63,12 @@ TokenType getTokenTypeByNum (int finalStateNum)
     case 19: return COMMA;
   }
 
-  // DEBUG
-  cout << "ERROR: " << finalStateNum << " is invalid!\n\n"; 
-
-  // sole purpose is to get read of warning for not returning something in the function
-  // (will not reach here...should not)
-  return (TokenType) finalStateNum;
+  return ERROR; //not at an final state
 }
 
+// helper function that returns the corresponding reserved keyword TokenType 
 TokenType getReservedTokenType(int index)
 {
-  
-  
-
-  // if ( val == reserved[0]) return INTEGER;
-  // else if (val == reserved[1]) return FLOAT;
-  // else if (val == reserved[2]) return WHILE;
-  // else if (val == reserved[3]) return IF;
-  // else if (val == reserved[4]) return THEN;
-  // else if (val == reserved[5]) return ELSE;
-  // else if (val == reserved[6]) return VOID;
-  // else if (val == reserved[7]) return BEGIN;
-  // else if (val == reserved[8]) return END;
-
   switch (index)
   {
     case 0: return INTEGER;
@@ -99,8 +82,10 @@ TokenType getReservedTokenType(int index)
     case 8: return END;
   }
 
-  //DEBUG
+  //Error message
   cout << "ERROR: Could not find a match!\n\n";
+
+  return ERROR;
 }
 
 
@@ -135,9 +120,8 @@ Token::print(ostream& os) const
  ******************************************************/
 void Token::get(istream &is)
 {
-  // you must write this code !!!!
 
-  //DFA table does not exists yet ...
+  //Create a DFA table if one does not already exists
   if (!DFA)
   {
     DFA = new int*[19+1]; // 19 states + the start state
@@ -147,12 +131,12 @@ void Token::get(istream &is)
     {
       DFA[state] = new int[256]; // new column at each row (state) that contains all the possible characters
 
-      //initalize each column to ERROR
+      // initalize each column to ERROR
       for (int ch=0; ch<256; ch++)
         DFA[state][ch] = -1; // -1 = ERROR
     }
     
-    // Mapping out graph 
+    // Mapping out graph (using the numbers on the graph)
 
     DFA[0][ (int) ',' ] = 19;   // START (0) -> , (19)
     DFA[0][ (int) ';' ] = 16;   // START (0) -> ; (16)
@@ -186,136 +170,117 @@ void Token::get(istream &is)
       DFA[12] [ (int) '&' ] = 13;    //START (0) -> & (13)
 
 
-    // could optimize...
+    //DIGIT
     for (char ch='0'; ch<='9'; ch++)
-      DFA[0][ (int) ch ] = 2;   // START (0) -> 0,1,...,9 denoted are DIGIT = 2
-
-      for (char ch='0'; ch<='9'; ch++)
+    {
+      DFA[0][ (int) ch ] = 2;   // START (0) -> 0,1,...,9 are DIGIT = 2
         DFA[2][ (int) ch ] = 2;   // DIGIT (2) -> DIGIT (2) 
+        DFA[2][ (int) '.' ] = 3;   // DIGIT (2) -> . (3)
+          DFA[3][ (int) ch ] = 4;   // . (3) -> DIGIT (4)
+            DFA[4][ (int) ch ] = 4;   // DIGIT (4) -> DIGIT (4)
+    }
 
-      for (char ch='0'; ch<='9'; ch++)
-        DFA[2] [ (int) '.' ] = 3;   // DIGIT (2) -> . (3)
+   
 
-          for (char ch='0'; ch<='9'; ch++)
-            DFA[3][ (int) ch ] = 4;   // . (3) -> DIGIT (4)
-
-            for (char ch='0'; ch<='9'; ch++)
-              DFA[4][ (int) ch ] = 4;   // DIGIT (4) -> DIGIT (4)
-
-                                                                                                                            
-    // could optimize... (lowercase letters)
+                                                                      
+    // ALPHA (lowercase letters)
     for (char ch='a'; ch<='z'; ch++)
+    {
       DFA[0][ (int) ch ] = 1;   // START (0) -> ALPHA (1)
-    
-      for (char ch='a'; ch<='z'; ch++)
         DFA[1][ (int) ch ] = 1;   // ALPHA (1) -> ALPHA (1)
     
       for (char digit='0'; digit<='9'; digit++)
         DFA[1][ (int) digit ] = 1;  //ALPHA (1) -> DIGIT (1)
+    }
 
 
-    // could optimize... (uppercase letters)
+    // ALPHA (uppercase letters)
     for (char ch='A'; ch<='Z'; ch++)
+    {
       DFA[0][ (int) ch ] = 1;   // START (0) -> ALPHA (1)
-    
-      for (char ch='A'; ch<='Z'; ch++)
         DFA[1][ (int) ch ] = 1;   // ALPHA (1) -> ALPHA (1)
     
       for (char digit='0'; digit<='9'; digit++)
         DFA[1][ (int) digit ] = 1;  //ALPHA (1) -> DIGIT (1)
+    }
 
   } //finish initializing table
   
 
   // keep track of the state
-  int currState = 0; /*PLACE HOLDER*/          // ** still do not know the number for start state
-  int prevState = -1;  // at start, no previous state
+  int currState = 0;   // 0 = the start state      
+  int prevState = -1;  // -1 = error (at start, there were no previous state)
 
+  _value = ""; 
+  char ch; // hold value read from input file
+  ch = is.get(); // get next char from input file
 
-  _value = ""; // hold value read from input file
-  char ch;
-
-  // ** should probably think about skipping white space here **
-  ch = is.get();// get next char from input
-
-  //DEBUG
-  // cout << "ch: " << ch << ".\n\n";
-  
   //_line_num is initalized to zero, but file number starts at 1
   if (_line_num == 0)
     _line_num++;
 
-  //NEED TO HANDLE IF IT IS A COMMENT #
-  while (isspace(ch) || isComment(ch)) // || is isComment() ...
-  {
-    //DEBUG
-    //cout << "Token.cpp: isspace    ch: " << ch << ".\n\n";
 
-    if (ch=='\n') // char read was a new line
+  //Handle white spaces and comments
+  while (isspace(ch) || isComment(ch))
+  {
+
+    //encountered a new line. Update the line number
+    if (ch=='\n')
       _line_num++;
 
-
-    //check if it is a comment
+    //encountered a comment
     if (isComment(ch))
     {
-      _line_num++;
-      // cout << "Encountered a comment!\n\n";
+      _line_num++; // account for the line skipped
+
       string commentStmt;
       getline(is, commentStmt); // "skip the values of the comment"
-      // cout << "commentStmt: " << commentStmt << endl;
     }
 
-    ch = is.get();
-    // cout << "ch: " << ch << "\n\n";
-
+    ch = is.get(); // get the next character...if possible
   }
 
+  // check if we are not at the end of the file
   if (!is)
   {
     _type = EOF_TOK;    
-    return;                //NEED TO FIX
+    return;
   }
 
+  // accounts for the extra character read from line 240
   is.putback(ch); 
-  //cout << "Token.cpp: isspace putback    ch: " << ch << "\n\n";
+  
 
 
-  //the algorithm
+  // the algorithm
   while(currState!=-1) // -1 denotes ERROR
   {
-    ch = is.get(); // get next char from input
-
-    //DEBUG
-    //cout << "Token.cpp: while()    ch: " << ch << "\n\n";
+    ch = is.get(); // get next char from input file
 
     // move to next state based on character read
     prevState = currState;
     currState = DFA[currState][(int) ch];
 
     if (currState!=-1) // if character is a valid part of token ...
-      _value+=ch; // ... add char to lexem's value
-
-
-    // need to conssider end of file token...
+      _value+=ch; // ... add char to lexeme's value
 
   }
   
-  // hopped out because we have encountered a valid state (ERROR)
-
-  
-  _type = getTokenTypeByNum(prevState); 
+  // hopped out because we have encountered a invalid state (ERROR)
+  _type = getTokenTypeByNum(prevState); // assign the TokenType of the _value 
 
   // make sure ID is not a reserved word
   if (_type == getTokenTypeByNum(1) /* 1=ID */)
   {
-    for (int i=0; i<9; i++) // check if it is a reserved keyword
+    for (int index=0; index<9; index++) // check if it is a reserved keyword from our set
     {
-      if (_value == reserved[i]) // it is a reserved keyword
-        _type = getReservedTokenType(i); // change the type to the reserve token type
+      if (_value == reserved[index]) // it is a reserved keyword
+        _type = getReservedTokenType(index); // change the type to the reserve token type
     }
   }
 
-  // we read an extra character ... put it back for the next read()
+  // we read an extra character ... put it back for the next get()
   if (is) // insure we are not at the end of the line
     is.putback(ch);
+
 }
